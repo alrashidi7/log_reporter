768ec9b2d5d43ee3a0034afc9b765dd2d45b3d65
Mahmoud alrashidi <147242051+ALRASHIDI-D@users.noreply.github.com>
2026-01-15 13:25:18 +0300

    * working on package

--- lib/src/exports.dart
+++ lib/src/exports.dart
@@ -1,5 +1,6 @@
 export 'core/reporter.dart';
 export 'core/reporter_config.dart';
+export 'core/log_levels.dart';
 
 export 'dio/api_metrics_interceptor.dart';
 export 'navigation/screen_observer.dart';
@@ -7,5 +8,8 @@ export 'cubit/logged_cubit.dart';
 
 export 'report/daily_report_builder.dart';
 export 'report/report_sender.dart';
+export 'report/daily_email_scheduler.dart';
+export 'report/smart_daily_report_builder.dart';
+export 'report/smart_email_reporter.dart';
 
 export 'ui/reporter_screen.dart';
