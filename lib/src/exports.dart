f4d498b552c36ccc493c5baa6e3410da533df002
Mahmoud alrashidi <147242051+ALRASHIDI-D@users.noreply.github.com>
2026-01-21 12:01:27 +0300

    *

--- lib/src/exports.dart
+++ lib/src/exports.dart
@@ -2,15 +2,17 @@ export 'core/reporter.dart';
 export 'core/reporter_config.dart';
 export 'core/decision_engine.dart';
 export 'core/log_levels.dart';
+export 'core/app_lifecycle_handler.dart';
 
 export 'dio/api_metrics_interceptor.dart';
 export 'navigation/screen_observer.dart';
 export 'cubit/logged_cubit.dart';
-export 'report/report_sender.dart';
 
+export 'report/report_sender.dart';
 export 'report/smart_daily_report_builder.dart';
 export 'report/smart_email_reporter.dart';
 
 export 'ui/reporter_screen.dart';
+
 export 'model/screen_log.dart';
 export 'model/api_impact_summary.dart';
