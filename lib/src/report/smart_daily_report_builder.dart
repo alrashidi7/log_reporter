a32fa3c7e0b41b7b4fe76d52172d3b672bf41c8b
Mahmoud alrashidi <147242051+ALRASHIDI-D@users.noreply.github.com>
2026-01-21 09:52:01 +0300

    * handle

--- lib/src/report/smart_daily_report_builder.dart
+++ lib/src/report/smart_daily_report_builder.dart
@@ -71,8 +71,10 @@ class SmartDailyReportBuilder {
         final message = errors[i].message ?? '';
         final match = apiErrorRegex.firstMatch(message);
         if (match == null) {
-          buffer.writeln('• ${errors[i].message} ');
-          buffer.writeln('     ${errors[i].message} ');
+          buffer.writeln(
+            '• ${errors[i].message} StackTrace: ${errors[i].stackTrace}',
+          );
+          buffer.writeln('      ');
         } else {
           groupApi(errors);
         }
